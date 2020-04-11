61f05271085312e5710bce84d9223fee01e98729
Keith Lawless <keithlawless@gmail.com>
2020-04-11 17:49:24 -0400

    Initial commit

---
+++ setenv.cmd
@@ -0,0 +1,2 @@
+set GRADLE_HOME=c:\Gradle\gradle-6.3-bin\gradle-6.3
+set PATH=%GRADLE_HOME%\bin;%PATH%
---
+++ src/main/java/com/keithlawless/demo/FirstPlugin.java
@@ -0,0 +1,7 @@
+package com.keithlawless.demo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FirstPlugin extends JavaPlugin {
+    
+}
